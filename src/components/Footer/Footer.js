9d19f1e6c35b02eba93157c9a70f3b3f0167fbfd
TGHeadle1371 <tgheadle@gmail.com>
2019-04-08 16:06:34 -0600

    Almost final

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="colors position-relative footer footer-100 footer-mgn navbar p-0">
+      <span className="text-muted" />
+      <div className="copyright"> &copy; 2019 | <a href="https://github.com/TGHeadle1371/clicky-game">Thomas Headle</a></div>
+    </div>
+  );
+}
+
+export default Footer;
